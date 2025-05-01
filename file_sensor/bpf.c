9eeea0ab6864ef5f912495cd376d080ba62d8d18
SMJ <smj00316@hanyang.ac.kr>
2025-05-02 01:56:57 +0900

    feat: Detect file open at file sensor

---
+++ file_sensor/bpf.c
@@ -0,0 +1,73 @@
+//go:build ignore
+
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+
+#ifndef TASK_COMM_LEN
+#define TASK_COMM_LEN 16
+#endif
+
+#define MAX_SIZE 128
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+struct fileOpenEvent {
+	u32 pid;
+	u8 name[MAX_SIZE];
+	u32 len;
+	u32 flags;
+	u16 mode;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_RINGBUF);
+	__uint(max_entries, 1 << 24);
+	__type(value, struct fileOpenEvent);
+} events SEC(".maps");
+
+void createFileOpenEvent(pid_t pid, const char* filename, int flags, umode_t mode){
+	struct fileOpenEvent *task_info;
+
+	task_info = bpf_ringbuf_reserve(&events, sizeof(struct fileOpenEvent), 0);
+	if (!task_info) {
+		return;
+	}
+
+	task_info->pid = pid;
+	task_info->len = bpf_core_read_str(&task_info->name, sizeof(task_info->name), filename);
+	task_info->flags = flags;
+	task_info->mode = mode;
+	
+	bpf_ringbuf_submit(task_info, 0);
+}
+
+
+//ProcessCreate
+SEC("kprobe/do_sys_openat2")
+int BPF_KPROBE(kprobe_do_sys_openat2, int dfd, const char *filename, struct open_how *how)
+{
+	pid_t pid = bpf_get_current_pid_tgid() >> 32;
+	u64 flags = BPF_CORE_READ(how, flags);
+	u64 mode = BPF_CORE_READ(how, mode);
+    // bpf_printk("KPROBE ENTRY OPEN: pid = %d, filename = %s, flag = %d, mode = %d\n", pid, filename, flags, mode);
+
+	struct fileOpenEvent *task_info;
+
+	task_info = bpf_ringbuf_reserve(&events, sizeof(struct fileOpenEvent), 0);
+	if (!task_info) {
+		return 0;
+	}
+
+	task_info->pid = pid;
+	task_info->len = bpf_probe_read_str(task_info->name, sizeof(task_info->name), filename);
+	task_info->flags = flags;
+	task_info->mode = mode;
+	
+	bpf_ringbuf_submit(task_info, 0);
+
+	// createFileOpenEvent(pid, filename, flags, mode);
+    return 0;
+}
--- file_sensor/file_sensor.go
+++ file_sensor/file_sensor.go
@@ -1,11 +1,86 @@
 package file_sensor
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -tags linux file file.bpf.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 bpf bpf.c -- -I../include
 
-func FileSensorStart() {
-	go start()
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/ringbuf"
+)
+
+func FileSensorStart(end chan bool) {
+	start()
+	end <- true
 }
+func panicf(s string, i ...interface{}) { panic(fmt.Sprintf(s, i...)) }
 
 func start() {
+	// Subscribe to signals for terminating the program.
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
 
+	// Load pre-compiled programs and maps into the kernel.
+	objs := bpfObjects{}
+	if err := loadBpfObjects(&objs, nil); err != nil {
+		log.Fatalf("loading objects: %v", err)
+	}
+	defer objs.Close()
+
+	kp, err := link.Kprobe("do_sys_openat2", objs.KprobeDoSysOpenat2, nil)
+	if err != nil {
+		log.Fatalf("opening kprobe: %s", err)
+	}
+	defer kp.Close()
+
+	// kp2, err := link.Kretprobe("do_unlinkat", objs.DoUnlinkatExit, nil)
+	// if err != nil {
+	// 	log.Fatalf("opening kprobe: %s", err)
+	// }
+	// defer kp2.Close()
+
+	rd, err := ringbuf.NewReader(objs.Events)
+	if err != nil {
+		log.Fatalf("opening ringbuf reader: %s", err)
+	}
+	defer rd.Close()
+
+	// Close the reader when the process receives a signal, which will exit
+	// the read loop.
+	go func() {
+		<-stopper
+
+		if err := rd.Close(); err != nil {
+			log.Fatalf("closing ringbuf reader: %s", err)
+		}
+	}()
+
+	log.Println("Waiting for events..")
+
+	var event bpfFileOpenEvent
+	for {
+		record, err := rd.Read()
+		if err != nil {
+			if errors.Is(err, ringbuf.ErrClosed) {
+				log.Println("Received signal, exiting..")
+				return
+			}
+			log.Printf("reading from reader: %s", err)
+			continue
+		}
+
+		// Parse the ringbuf event entry into a bpfEvent structure.
+		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
+			log.Printf("parsing ringbuf event: %s", err)
+			continue
+		}
+		// log.Printf("OPEN:: pid: %d, filename = %s, flag = %d, mode = %d\n", event.Pid, unix.ByteSliceToString(event.Name[:]), event.Flags, event.Mode)
+	}
 }
