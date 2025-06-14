8b9270b3a4488cb13909308e1ce38e56169674ab
SMJ <smj00316@hanyang.ac.kr>
2025-06-15 03:06:44 +0900

    Add shell sensor

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 //go:generate go generate ./file_sensor
 //go:generate go generate ./process_sensor
 //go:generate go generate ./tcp_sensor
+//go:generate go generate ./shell_sensor
 
 import (
 	"ebpf-monitor/file_sensor"
+	"ebpf-monitor/logger"
 	"ebpf-monitor/process_sensor"
+	"ebpf-monitor/shell_sensor"
 	"ebpf-monitor/tcp_sensor"
 	"os"
 	"os/signal"
@@ -16,6 +19,7 @@ import (
 )
 
 func main() {
+	logger.Init()
 
 	if err := rlimit.RemoveMemlock(); err != nil {
 		panic(err)
@@ -24,20 +28,27 @@ func main() {
 	file_sensor_signal := make(chan os.Signal, 1)
 	process_sensor_signal := make(chan os.Signal, 1)
 	tcp_sensor_signal := make(chan os.Signal, 1)
+	shell_sensor_signal := make(chan os.Signal, 1)
 
 	signal.Notify(file_sensor_signal, os.Interrupt, syscall.SIGTERM)
 	signal.Notify(process_sensor_signal, os.Interrupt, syscall.SIGTERM)
 	signal.Notify(tcp_sensor_signal, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(shell_sensor_signal, os.Interrupt, syscall.SIGTERM)
 
 	file_sensor_end := make(chan bool)
 	process_sensor_end := make(chan bool)
 	tcp_sensor_end := make(chan bool)
+	shell_sensor_end := make(chan bool)
 
 	go file_sensor.FileSensorStart(file_sensor_signal, file_sensor_end)
 	go process_sensor.ProcessSensorStart(process_sensor_signal, process_sensor_end)
 	go tcp_sensor.TcpSensorStart(tcp_sensor_signal, tcp_sensor_end)
+	go shell_sensor.SensorStart(shell_sensor_signal, shell_sensor_end)
 
 	<-file_sensor_end
 	<-process_sensor_end
 	<-tcp_sensor_end
+	<-shell_sensor_end
+
+	logger.OUTPUT <- nil
 }
