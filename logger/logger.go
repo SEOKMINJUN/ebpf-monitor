e48d1c46023edc9d98c26479222af0c0c4054e3f
SMJ <smj00316@hanyang.ac.kr>
2025-06-19 05:11:25 +0900

    Disable stdout log and use otel trace

--- logger/logger.go
+++ logger/logger.go
@@ -4,49 +4,54 @@ import (
 	"ebpf-monitor/helper"
 	"encoding/json"
 	"log"
-	"os"
-	"time"
 )
 
 var PRINT_OUTPUT = true
 var OUTPUT = make(chan []byte)
 
 func Init() {
+	helper.InitTracer()
 
-	f, err := os.OpenFile("output.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalf("Failed to open or create output log")
-		return
-	}
+	// f, err := os.OpenFile("output.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// if err != nil {
+	// 	log.Fatalf("Failed to open or create output log")
+	// 	return
+	// }
 
-	go func() {
-		var log_string []byte = nil
-		for {
-			log_string = <-OUTPUT
-			if log_string == nil || len(log_string) == 0 {
-				f.Close()
-				break
-			}
-			f.Write(log_string)
-			f.Write([]byte{0x0A})
-		}
+	// go func() {
+	// 	var log_string []byte = nil
+	// 	for {
+	// 		log_string = <-OUTPUT
+	// 		if log_string == nil || len(log_string) == 0 {
+	// 			f.Close()
+	// 			break
+	// 		}
+	// 		f.Write(log_string)
+	// 		f.Write([]byte{0x0A})
+	// 	}
 
-	}()
+	// }()
+}
+
+func Close() {
+	// helper.CloseTracer()
+	// OUTPUT <- nil
 }
 
 func LogEvent(event_type string, timestamp int64, obj interface{}) {
 	event := helper.Event{}
-	event.TYPE = event_type
-	event.TIMESTAMP = time.Unix(timestamp, 0).Format(time.RFC3339Nano)
-	event.Data = helper.StructToMap(obj)
+	event.SetInfo(event_type, timestamp, obj)
+	event.Handle()
+
 	//Do send trace
 }
 
 func WriteOutput(sensor_type int, jsonBytes []byte) {
 	if !json.Valid(jsonBytes) {
-		log.Fatalf("Get invalid json object from %s", sensor_type)
+		log.Fatalf("Get invalid json object from %d", sensor_type)
 	}
 
+	// Filter sensor
 	// if sensor_type == 0 {
 	// 	return
 	// }
