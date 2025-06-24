253b4d0bbe30096804381f07221eee986e7eb6cc
SMJ <smj00316@hanyang.ac.kr>
2025-06-25 04:51:50 +0900

    Fix shell readline

--- shell_sensor/bpf.c
+++ shell_sensor/bpf.c
@@ -11,6 +11,7 @@ struct readlineEvent {
 	u64 timestamp;
 	u32 pid;
 	u32 uid;
+	u8 comm[TASK_COMM_LEN];
 	u8 line[80];
 };
 
@@ -20,16 +21,19 @@ struct {
 	__type(value, struct readlineEvent);
 } readlineEvents SEC(".maps");
 
-SEC("uretprobe/bash_readline")
-int uretprobe_bash_readline(struct pt_regs *ctx) {
+SEC("uretprobe//bin/bash:readline")
+int BPF_KRETPROBE(uretprobe_bash_readline, const void *ret) {
 	struct readlineEvent event;
 
 	event.timestamp = bpf_ktime_get_ns();
 	event.pid = bpf_get_current_pid_tgid() >> 32;
 	event.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-	bpf_probe_read(&event.line, sizeof(event.line), (void *)PT_REGS_RC(ctx));
+ 	bpf_get_current_comm(&event.comm, sizeof(event.comm));
+	bpf_probe_read(&event.line, sizeof(event.line), ret);
 
 	bpf_perf_event_output(ctx, &readlineEvents, BPF_F_CURRENT_CPU, &event, sizeof(event));
+
+	bpf_printk("bash_readline\n");
 
 	return 0;
 }
